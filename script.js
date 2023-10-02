8e17ee7bc8e1ebfe625a37d9c5193806da18ce73
kirakr <82437235+kirakr@users.noreply.github.com>
2023-10-02 15:11:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 // JavaScript
 
-//console.log(document.getElementById('myColor').value);
-
-//定数の宣言
 const color = document.getElementById('myColor');
 const text = document.getElementById('bodyText');
 
-//document.getElementById('bodyText').textContent = document.getElementById('myColor').value;
 
-text.textContent = color.value;
+function changeColor(){
+  document.body.style.backgroundColor = color.value;
+
+
+  if (color.value === '#ffffff') {
+    text.textContent = 'コード：' + color.value + '(white)';
+  } else if (color.value === '#000000') {
+    text.textContent = 'コード：' + color.value + '(black)';
+  } else {
+    text.textContent = 'コード：' + color.value;
+  }
+
+}
+
+color.addEventListener('input', changeColor);
